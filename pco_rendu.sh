ec18c337aac431e5d5e0c063d03b032c5c4ce722
Bruno Carvalho <bruno.darochacarvalho@heig-vd.ch>
2025-09-23 16:39:22 +0200

    add report in rendu script

--- pco_rendu.sh
+++ pco_rendu.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+SCRIPT=${BASH_SOURCE[0]}
+SCRIPTPATH=$(realpath $(dirname $SCRIPT))
 PROJECT_DIR=common
 ARCHIVE=rendu.tar.gz
+REPORT_FILE=rapport.pdf
 
 if [ ! -d "$PROJECT_DIR" ]
 then
@@ -9,6 +12,13 @@ then
     exit 1
 fi
 
+if [ ! -f "$REPORT_FILE" ]
+then
+    echo "Could not find $REPORT_FILE directory in $SCRIPTPATH" >&2
+    exit 1
+fi
+
+
 echo "The following files are archived in $ARCHIVE : "
-tar --exclude='rendu.tar.gz' --exclude='*.o' --exclude='*.user' -czvf $ARCHIVE $PROJECT_DIR
+tar --exclude='rendu.tar.gz' --exclude='*.o' --exclude='*.user' -czvf $ARCHIVE ${PROJECT_DIR} $REPORT_FILE
 
