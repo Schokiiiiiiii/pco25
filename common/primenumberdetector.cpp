5741b34674bc67108187d95f1bbce94ec1c2f016
albonny <aymeric.bonny@heig-vd.ch>
2025-10-01 11:17:05 +0200

    sequential prime

--- common/primenumberdetector.cpp
+++ common/primenumberdetector.cpp
@@ -3,7 +3,11 @@
 #include "primenumberdetector.h"
 
 bool PrimeNumberDetector::isPrime(uint64_t number) {
-    // TODO
+    uint64_t upper_bound = static_cast<uint64_t>((sqrt(number)); //casting a double to int rounds it towards 0
+    while (upper_bound) { // the square root of the max 64 bits value is in the 10⁹ range
+        if (!(number % upper_bound)) return true;
+        --upper_bound;
+    }
     return false;
 }
 
