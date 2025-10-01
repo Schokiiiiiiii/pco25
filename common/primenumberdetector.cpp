30c79f66497c937b132df1b91fc21e8c15cf5b37
albonny <aymeric.bonny@heig-vd.ch>
2025-10-01 11:39:44 +0200

    parenthesis fix

--- common/primenumberdetector.cpp
+++ common/primenumberdetector.cpp
@@ -3,7 +3,7 @@
 #include "primenumberdetector.h"
 
 bool PrimeNumberDetector::isPrime(uint64_t number) {
-    uint64_t upper_bound = static_cast<uint64_t>((sqrt(number)); //casting a double to int rounds it towards 0
+    uint64_t upper_bound = static_cast<uint64_t>((sqrt(number))); //casting a double to int rounds it towards 0
     while (upper_bound) { // the square root of the max 64 bits value is in the 10⁹ range
         if (!(number % upper_bound)) return true;
         --upper_bound;
