e7bf9ba7806d828cb3eef074e32c85e2378f890b
Schokiiiiiiii <dkcfab@gmail.com>
2025-10-08 13:29:06 +0200

    Ex2 mostly done but not working

--- common/primenumberdetector.cpp
+++ common/primenumberdetector.cpp
@@ -2,6 +2,8 @@
 
 #include "primenumberdetector.h"
 
+#include <vector>
+
 bool PrimeNumberDetector::isPrime(uint64_t number) {
     auto upper_bound = static_cast<uint64_t>(sqrt(number)); //casting a double to int rounds it towards 0
     while (upper_bound > 1) { // the square root of the max 64 bits value is in the 10⁹ range
@@ -12,10 +14,53 @@ bool PrimeNumberDetector::isPrime(uint64_t number) {
 }
 
 PrimeNumberDetectorMultiThread::PrimeNumberDetectorMultiThread(size_t nbThreads) {
-    // TODO
+    if (nbThreads < 1) throw std::invalid_argument("Invalid number of threads!");
+    this->nbThreads = nbThreads;
 }
 
 bool PrimeNumberDetectorMultiThread::isPrime(uint64_t number) {
-    // TODO
-    return false;
+
+    // initialize thread vector & result
+    std::vector<PcoThread*> threads;
+    bool isPrime = true;
+
+    // calculate square root of number
+    auto sqrt_num = static_cast<uint64_t>(sqrt(number));
+
+    // find range per thread and start
+    auto size_range = static_cast<uint64_t>(sqrt_num / nbThreads);
+    uint64_t start = 2;
+
+    // initialize all threads
+    for (size_t i = 0 ; i < nbThreads - 1 ; ++i) {
+        auto* thread = new PcoThread(isPrimeRange, number, start, start + size_range, isPrime);
+        threads.push_back(thread);
+        start += size_range;
+    }
+    // initialize last thread
+    auto* last_thread = new PcoThread(isPrimeRange, number, start, sqrt_num, isPrime);
+    threads.push_back(last_thread);
+
+    // wait for all threads to join
+    for (size_t i = 0 ; i < nbThreads ; ++i) {
+        threads[i]->join();
+    }
+
+    // delete all threads
+    for (size_t i = 0 ; i < nbThreads ; ++i) {
+        delete threads[i];
+    }
+
+    // return result
+    return isPrime;
+}
+
+void PrimeNumberDetectorMultiThread::isPrimeRange(const uint64_t number,
+                                                  const uint64_t lower,
+                                                  uint64_t upper,
+                                                  bool& isPrime) {
+    while (upper >= lower) {
+        if (!(number % upper)) isPrime = false;
+        --upper;
+    }
 }
