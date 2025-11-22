84f673acfaad2f5c068cc776bbfec04cd1a85d87
albonny <aymeric.bonny@heig-vd.ch>
2025-11-22 15:16:36 +0100

    apparement ca avait pas commit?

--- code/src/locomotivebehavior.h
+++ code/src/locomotivebehavior.h
@@ -58,9 +58,15 @@ protected:
      */
     std::shared_ptr<SharedSectionInterface> sharedSection;
 
-    // Le sens 0 est le sens dans lequuel les locos commencent, càd le sens horaire
-    std::pair<u_short, u_short> amauryContacts[1] = {{0, 12}};
-    std::pair<u_short, u_short> borisContacts[0] = {};
+    // Le sens D1 est le sens dans lequel les locos commencent, càd le sens horaire
+    std::array<std::pair<SharedSectionInterface::Direction, u_short>, 4> aContacts = {{SharedSectionInterface::Direction::D1, 12},
+                                                                                      {SharedSectionInterface::Direction::D1, 4},
+                                                                                      {SharedSectionInterface::Direction::D2, 5},
+                                                                                      {SharedSectionInterface::Direction::D2, 11}};
+    std::array<std::pair<SharedSectionInterface::Direction, u_short>, 4> bContacts = {{SharedSectionInterface::Direction::D1, 10},
+                                                                                      {SharedSectionInterface::Direction::D1, 4},
+                                                                                      {SharedSectionInterface::Direction::D2, 3},
+                                                                                      {SharedSectionInterface::Direction::D2, 11}};
 
     /*
      * Vous êtes libres d'ajouter des méthodes ou attributs
